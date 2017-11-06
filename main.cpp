a19b19e218a52e77bf200af01f2bfca29165fe9c
Roman <exculibur@rambler.ru>
2017-11-06 04:17:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,67 @@
-#include <string>
-#include <cstring>
-#include <iostream>
-#include <fstream>
 #include <boost/filesystem.hpp>
+#include <cstring>
+#include <fstream>
 #include <future>
-
+#include <iostream>
 
 using namespace std;
 namespace fs = boost::filesystem;
 
-string EXPLORER_LOG_PATH = "log.txt";
-
-//Подсчет размера директории
-double getDirSize(string dirAddress, double size) {
-    fs::path dirPath = dirAddress;
-    fs::path textDir = dirPath.filename();
-    cout << "Finding size of " << textDir << ". Please, wait...\n";
-    if(fs::exists(dirPath)){
-      fs::directory_iterator end_itr;
-      for(fs::directory_iterator dirIte(dirPath); dirIte != end_itr; ++dirIte) {
-        fs::path filePath(dirIte->path());
-        try {
-          if(!fs::is_directory(dirIte->status()))
-          {
-            size = size + fs::file_size(filePath);
-          }
-          else {
-            getDirSize(filePath.string(), size);
-          }
-        }
-        catch(exception& e){
-          cout << e.what() << endl;
-        }
-        //Проверка на то, является ли файл, размер которого мы хотим узнать, директорией
-      }
-    }
-    return size;
-}
-
-string getDirName(string dirAddress) {
-    cout << "Finding name of file or directory. Please, wait...\n" << endl;
-    fs::path dirPath = dirAddress;
-    fs::path textDir = dirPath.filename();
-    return textDir.string();
-}
-
-string getDirData(string dirAddress) {
-    cout << "Finding latest modification data. Please, wait...\n" << endl;
-    fs::path dirPath = dirAddress;
-    std::time_t ftime = fs::last_write_time(dirPath);
-    return asctime(gmtime(&ftime));
-}
-
-void displayDir(string dirAddress) {
-    //Проверка на существование директории
-    if(!fs::exists(dirAddress)) {
-      throw runtime_error("ERROR: We don't have such directory");
-    }
-    //Проверка на то, является ли файл директорией
-    if(!fs::is_directory(dirAddress)) {
-      throw runtime_error("ERROR: Type of file - not directory");
-    }
-
-    std::future<string> futureDirName = std::async (getDirName,dirAddress);
-    std::future<string> futureDirData = std::async (getDirData,dirAddress);
-    std::future<double> futureDirSize = std::async (getDirSize,dirAddress, 0);
-
-    string dirName = futureDirName.get();
-    string dirData = futureDirData.get();
-    double dirSize = futureDirSize.get();
-
-    //Запись данных о директории в файл
-    ofstream fout(EXPLORER_LOG_PATH);
-    fout << "Directory name: " << dirName << endl;
-    fout << "Latest modification: " << dirData << endl;
-    fout << "Directory size: " << dirSize/1000 << " Kb" << endl;
-    fout.close();
-
-}
-
-
-int main(int argc, char* argv[])
+string getFileName(string address)
 {
-  printf("%d \n", argc);
-  string ad;
-  ad = argv[1];
+	fs::path filePath = address;
+	fs::path textFilename = filePath.filename();
+	return textFilename.string();
+}
 
-  try {
-    displayDir(ad);
-  }
-  catch(const exception& e) {
-    cout << e.what() << endl;
-  }
+double getFileSize(string address)
+{
+	fs::path filePath = address;
+	return fs::file_size(filePath);
+}
+
+string getFileDate(string address)
+{
+	fs::path filePath = address;
+	time_t ftime = fs::last_write_time(filePath);
+	return asctime(gmtime(&ftime));
+}
+
+void Get(string address)
+{
+
+	if (fs::is_directory(address))
+		throw runtime_error("Type of file - directory");
+
+	if (!fs::exists(address))
+		throw runtime_error("File does not find!");
+
+	future<string>futureFileName = async(getFileName, address);
+	future<double>futureFileSize = async(getFileSize, address);
+	future<string>futureFileDate = async(getFileDate, address);
+
+	string FileName = futureFileName.get();
+	double FileSize = futureFileSize.get();
+	string FileDate = futureFileDate.get();
+	
+	cout << "Название файла:\t " << FileName << endl;
+	cout << "Размер:\t " << FileSize << " byte" << endl;
+	cout << "Modification date:\t " << FileDate << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	try
+	{
+		string ad;
+		cout << "Укажите путь к файлу" << endl;
+		cin >> ad;
+
+		Get(ad);
+	}
+	catch (const exception& e)
+	{
+		cout << e.what() << endl;
+	}
 }
